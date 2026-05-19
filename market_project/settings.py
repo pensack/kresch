6ebfbf87077d215019683ab44fdd2efa55a4b5c1
unknown <mark.mainac@gmail.com>
2026-05-19 05:41:50 +0300

    chore: add dj-database-url for production database flexibility

--- market_project/settings.py
+++ market_project/settings.py
@@ -93,12 +93,19 @@ WSGI_APPLICATION = 'market_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
